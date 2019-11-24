0d31b7eb68be63d3ea763c3f037d432d01a45e71
Adash <adash108@yahoo.com>
2019-11-24 01:37:57 +0000

    many changes :)

--- src/AddTaskForm.js
+++ src/AddTaskForm.js
@@ -1,65 +1,63 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
+const initialState = {
+  title: "",
+  text: "",
+  completed: false
+};
 
 class AddTaskForm extends Component {
-    constructor(props){
-        super(props);
+  state = initialState;
 
-        this.initialState = {
-            title: '',
-            text: '',
-            completed: false,
-        }
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
 
-        this.state = this.initialState;
+  submitForm = event => {
+    event.preventDefault();
+    this.props.handleSubmit(this.state);
+    this.setState(initialState);
+  };
 
-        this.handleChange = this.handleChange.bind(this);
-        this.submitForm = this.submitForm.bind(this);
-    }
+  render() {
+    const { title, text } = this.state;
 
-    handleChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value,
-        });
-    }
-
-    submitForm = (event) => {
-        event.preventDefault();
-        this.props.handleSubmit(this.state);
-        this.setState(this.initialState);
-    }
-
-    render() {
-        const { title, text } = this.state;
-
-        return (
-            <form className="commentForm" onSubmit={ this.submitForm } >
-            <div className="form-group">   
-            <label>Task</label>
-            <input 
-                type="text" 
-                name="title" 
-                className="form-control"
-                value={ title } 
-                onChange={ this.handleChange } />
-            </div>        
-            <div className="form-group">
-            <label>Task notes</label>
-            <input 
-                type="text" 
-                name="text" 
-                className="form-control"
-                value={ text } 
-                onChange={ this.handleChange }/>
-            </div>    
-            <button
-                type="submit" 
-                className="btn btn-primary"
-                value="Submit" 
-                onClick={this.submitForm} > submit </button>
-        </form>
-        )
-    }
+    return (
+      <form className="commentForm" onSubmit={this.submitForm}>
+        <div className="form-group">
+          <label>Task</label>
+          <input
+            type="text"
+            name="title"
+            className="form-control"
+            value={title}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Task notes</label>
+          <input
+            type="text"
+            name="text"
+            className="form-control"
+            value={text}
+            onChange={this.handleChange}
+          />
+        </div>
+        <button
+          type="submit"
+          className="btn btn-primary"
+          value="Submit"
+          onClick={this.submitForm}
+        >
+          {" "}
+          submit{" "}
+        </button>
+      </form>
+    );
+  }
 }
 
 export default AddTaskForm;
--- src/App.js
+++ src/App.js
@@ -1,83 +1,83 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import AddTaskForm from './AddTaskForm';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import React, { Component } from "react";
+import AddTaskForm from "./AddTaskForm";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
+const initialState = [
+  {
+    title: "first task",
+    text: "text of the first todo",
+    completed: false
+  },
+  {
+    title: "second task",
+    text: "text of the second todo",
+    completed: true
+  }
+];
 
+const TasksList = props => {
+  const handleClick = key => {
+    props.handleListClick(key);
+  };
 
-const TasksList = (props) => {
-  const handleClick = (key) => { props.handleListClick(key) };
-
-  const tasks = props.notes.map((note, index)=>{
-    const completed = (note.completed ? "line-through" : "");
+  const tasks = props.notes.map((note, index) => {
+    const completed = note.completed ? "line-through" : "";
     return (
-      <li key={ index } 
-          className="list-group-item" 
-          onClick={ (key)=>handleClick(index)} 
-          style={{ textDecoration: completed }}
-          >
-            <span style={{ color: "darkgreen" }}> 
-              { note.title } 
-            </span> 
-              -- 
-            <span style={{ color: "seagreen" }}>
-              { note.text }
-            </span>
+      <li
+        key={index}
+        className="list-group-item"
+        onClick={key => handleClick(index)}
+        style={{ textDecoration: completed }}
+      >
+        <p>
+          <span style={{ color: "darkgreen" }}>{note.title}</span>
+        </p>
+        <p>
+          <span style={{ color: "seagreen" }}>{note.text}</span>
+        </p>
       </li>
-    )
+    );
   });
-  
-  return <ul className="list-group" >{ tasks }</ul>
-}
+
+  return <ul className="list-group">{tasks}</ul>;
+};
 
 class App extends Component {
-  constructor(props){
-    super(props);
+  state = {
+    notes: initialState
+  };
 
-    this.state = {
-      notes: [
-        {title: 'first task', text: 'text of the first todo', completed: false,},
-        {title: 'second task', text: 'text of the second todo', completed: true,}
-      ],
-    }
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleListClick = this.handleListClick.bind(this);
-  }
-
-  handleSubmit = (value) => {
+  handleSubmit = value => {
     this.setState({
-      notes: [...this.state.notes, value],
+      notes: [...this.state.notes, value]
     });
-  }
+  };
 
-  handleListClick = (key)=> {
+  handleListClick = key => {
     const notesCopy = this.state.notes.slice();
-    const completed = ( this.state.notes[key].completed ? false : true );
+    const completed = this.state.notes[key].completed ? false : true;
     notesCopy[key].completed = completed;
 
     this.setState({
-      notes : notesCopy 
+      notes: notesCopy
     });
-  }
+  };
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
+        <header className="App-header"></header>
         <div className="container">
           <div className="row">
             <div className="col-6">
-                <TasksList 
-                  notes={ this.state.notes } 
-                  handleListClick={ this.handleListClick } 
-                />
+              <TasksList
+                notes={this.state.notes}
+                handleListClick={this.handleListClick}
+              />
             </div>
             <div className="col-6">
-              <AddTaskForm handleSubmit={ this.handleSubmit } />
+              <AddTaskForm handleSubmit={this.handleSubmit} />
             </div>
           </div>
         </div>
